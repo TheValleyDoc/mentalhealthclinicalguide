f1010cec095863704e6399bc7a26be086c5de04b
John Craig <johnpatrickcraig@outlook.com>
2026-01-10 19:31:26 -0500

    Fix homepage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navigation } from './components/Navigation';
-import { HomePage } from './components/HomePage';
-import { DisorderPage } from './components/DisorderPage';
-import { annapolisValleyData } from './data/annapolisValleyData';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigation } from "./components/Navigation";
+import { HomePage } from "./components/HomePage";
+import { DisorderPage } from "./components/DisorderPage";
+import { annapolisValleyData } from "./data/annapolisValleyData";
 
 export default function App() {
   return (
@@ -10,12 +10,16 @@ export default function App() {
       <div className="min-h-screen bg-gray-50">
         <Navigation />
         <Routes>
+          <Route path="/mentalhealthclinicalguide" element={<HomePage />} />
           <Route path="/" element={<HomePage />} />
           <Route
             path="/depression"
             element={<DisorderPage data={annapolisValleyData.depression} />}
           />
-          <Route path="/anxiety" element={<DisorderPage data={annapolisValleyData.anxiety} />} />
+          <Route
+            path="/anxiety"
+            element={<DisorderPage data={annapolisValleyData.anxiety} />}
+          />
           <Route
             path="/substance-use"
             element={<DisorderPage data={annapolisValleyData.substanceUse} />}
